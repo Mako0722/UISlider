4059c9a136e70b6128d83d1e58da1378a28a696f
Mako0722 <zhongtiancheng79@gmail.com>
2022-12-03 20:52:37 +0900

    first commit

---
+++ UISlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISlider
+//
+//  Created by 中田誠 on 2022/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var flower: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
