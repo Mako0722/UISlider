a2d831ff4678005824a37ced13f4b7169001f0de
Mako0722 <zhongtiancheng79@gmail.com>
2022-12-03 21:02:16 +0900

    UISlider

--- UISlider/ViewController.swift
+++ UISlider/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var flower: UIImageView!
     
+
+    @IBAction func changeSlider(_ sender: UISlider) {
+        flower.alpha = CGFloat(sender.value)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
